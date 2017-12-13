665caac4b5158df99988f30df52e537f477345e2
sultanassi95 <mrsultanassi@gmail.com>
2017-12-13 09:38:20 +0200

    adding config and rearranging structure order.

--- src/db/database/build.js
+++ src/db/database/build.js
@@ -1,10 +1,11 @@
+const connect = require('./db_connection');
 const fs = require('fs');
 
-const dbConnection = require('./db_connection');
-
 const sql = fs.readFileSync(`${__dirname}/build.sql`).toString();
-
-dbConnection.query(sql, (err, res) => {
-  if (err) console.log(err);
-  else console.log(`response is ${res}`);
+connect.query(sql, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
 });
