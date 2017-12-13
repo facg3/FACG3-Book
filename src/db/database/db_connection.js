fb1d19b05d38cb395cdbf30b63f1d72d20db84a0
sultanassi95 <mrsultanassi@gmail.com>
2017-12-13 10:11:04 +0200

    fixing issues occured with new upgrades on database tests functions.

--- src/db/database/db_connection.js
+++ src/db/database/db_connection.js
@@ -1,9 +1,7 @@
 const { Pool } = require('pg');
 
-let dbUrl;
 require('env2')('./config.env');
 
 if (!process.env.DB_URL) { throw new Error('DB_URL is not founf'); }
-dbUrl = process.env.DB_URL;
-const pool = new Pool({ connectionString: dbUrl });
+const pool = new Pool({ connectionString: process.env.DB_URL });
 module.exports = pool;
